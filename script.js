7072cbe6b21779fdfdde99948963376ebe98d3c2
hass0n3 <hassen.alfaris@ocsbstudent.ca>
2021-03-24 14:38:57 +0000

    1

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 document.getElementById('button').addEventListener('click', randomizer)
-let ran = 0
-let user = 0
+let randomNumber = 0
+let userGuess = 0
+
 function randomizer () {
-  userGuess = document.getElementById('input').value
-  userGuess = parseInt(userGuess)
+userGuess= document.getElementById('input').value
+  userGuess= parseInt(userGuess)
 
   randomNumber = (Math.random() * 6) + 5
   randomNumber = parseInt(randomNumber)
-  if (userGuess === randomNumber) {
-    alert('Congratulation You Win !!!')
+
+  if (userGuess == randomNumber) {
+alert('Congragulation You Win !!!')
   }
 }
