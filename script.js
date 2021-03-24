189b061414026a014de40a737c089e62b0bfb09f
hass0n3 <hassen.alfaris@ocsbstudent.ca>
2021-03-24 14:48:36 +0000

    3

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-document.getElementById('button').addEventListener('click', randomizer)
-let randomNumber = 0
+// This code will display a message if the user correctly guesses the random number
 let userGuess = 0
+let randomNumber = 0
+
+document.getElementById('button').addEventListener('click', randomizer)
 
 function randomizer () {
-userGuess= document.getElementById('input').value
-  userGuess= parseInt(userGuess)
+  // convert userGuess to integer
+  userGuess = document.getElementById('input').value
+  userGuess = parseInt(userGuess)
 
-  randomNumber = (Math.random() * 6) + 5
-  randomNumber = parseInt(randomNumber)
+  // generate random number
+  randomNumber = Math.floor(Math.random() * 6) + 1
 
-  if (userGuess == randomNumber) {
-alert('Congragulation You Win !!!')
+  // compare user's guess to random number
+  if (userGuess === randomNumber) {
+    document.getElementById('answer').innerHTML = 'You guessed ' + userGuess + '. That is correct!'
   }
 }
